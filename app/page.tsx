770f33aee8685ad0cad6d6a4be7ecbf948be463f
MEHERAB HOSSAIN SHAFIN <261-35-113@diu.edu.bd>
2026-04-18 10:43:18 +0600

    Clean up page.tsx by removing unused code
    Removed unused imports and DemoOne component.

--- app/page.tsx
+++ app/page.tsx
@@ -13,17 +13,3 @@ export default function Home() {
     </div>
   );
 }
-
-import { HeroSection } from "@/components/ui/hero-3";
-import { Header } from "@/components/ui/header-3"; // @efferd/header-3
-
-export default function DemoOne() {
-	return (
-		<div className="flex w-full flex-col">
-		<Header />
-		  <main className="grow" >
-			<HeroSection />
-	      </main>
-	   </div>
-  );
-}
